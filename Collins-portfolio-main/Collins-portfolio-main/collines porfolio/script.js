10d028ab72e55dbc6bb6a7527e59c9d4d679faa7
Davs3447 <collinesonyango68@gmail.com>
2026-07-07 23:51:34 +0300

    Add files via upload

---
+++ Collins-portfolio-main/Collins-portfolio-main/collines porfolio/script.js
@@ -0,0 +1,25 @@
+// Mobile Menu
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// Back to Top Button
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
